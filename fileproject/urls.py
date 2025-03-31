00cfc3e598f3312670efbd9ffc7f03e8b2167e96
Ujjwal Neupane <itsmeujjwal725@gmail.com>
2025-03-31 12:18:05 +0545

    User Authentication with Djoser Token Authentication completed

--- fileproject/urls.py
+++ fileproject/urls.py
@@ -17,10 +17,19 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('/',include('Filedigital.urls')),
-    # path('user/',include('users.urls')),
+    path('',include('Filedigital.urls')),
+    path('user/',include('users.urls')),
+    
+    #Djoser urls
+    path("auth/", include("djoser.urls")),  # Registration, password reset, etc.
+    path("auth/", include("djoser.urls.authtoken")),  # Token authentication endpoints
     
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
